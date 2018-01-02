c3a927a0686b67fd13b3dffd73ad835b3d503881
Chase Steely <chasesteely@gmail.com>
2018-01-01 19:00:24 -0600

    all refactoring complete. MVP complete. All mojor functionality complete.

--- myApp/www/app/controllers/dashHomeController.js
+++ myApp/www/app/controllers/dashHomeController.js
@@ -3,13 +3,52 @@ angular.module('BeerApp')
         $scope.name = ""
         $scope.photo = ""
         $scope.uniqueBeers = 0
+        $scope.localBeers = 0
+        let uCount = []
+        let localCount = []
+   
+        //get beer count
+        function uniqueBeerCount(drinker) {
+            BeerFactory.getLoggedBeers(drinker.uid).then(data => {
+                $timeout(function () {
+                    console.log()
+                }, 100)
+                result = data.filter(b => b.wishlist === false)
+                for (i = 0; i < result.length; i++) {
+                    if (uCount.indexOf(result[i].data.beer.bid) === -1) {
+                        uCount.push(result[i].data.beer.bid);
+                    }
+                }
+                $scope.uniqueBeers = uCount.length
+                console.log(uCount)
+            })
+        }
 
-        
+        function localBeerCount(drinker) {
+            BeerFactory.getLoggedBeers(drinker.uid).then(data => {
+                $timeout(function () {
+                    console.log()
+                }, 100)
+                result = data.filter(b => b.wishlist === false)
+                for (i = 0; i < result.length; i++) {
+                    if (localCount.indexOf(result[i].data.beer.bid) === -1) {
+                        localCount.push(result[i]);
+                    }
+                }
+                console.log(localCount)
+                local = localCount.filter(b => b.data.beer.brewery.location.brewery_city === "Nashville" || b.data.beer.brewery.location.brewery_city === "Murfreesboro" || b.data.beer.brewery.location.brewery_city === "Franklin" || b.data.beer.brewery.location.brewery_city === "HENDERSONVILLE" || b.data.beer.brewery.location.brewery_city === "Nolensville" || b.data.beer.brewery.location.brewery_city === "Gallatin" || b.data.beer.brewery.location.brewery_city === "Nashville")
+                $scope.localBeers = local.length
+                console.log(local)
+            })
+        }
+
 
         function loadHome() {
             drinker = AuthFactory.getUser()
             $scope.name = drinker.displayName
             $scope.photo = drinker.photoURL
+            uniqueBeerCount(drinker)
+            localBeerCount(drinker)
         }
 
 
